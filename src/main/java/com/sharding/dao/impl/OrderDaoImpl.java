1f9bcb3ac3c8e6a7c0daebd942ec573f949006e1
yanlin <362460453@qq.com>
2019-06-03 13:48:07 +0800

    读写分离

--- src/main/java/com/sharding/dao/impl/OrderDaoImpl.java
+++ src/main/java/com/sharding/dao/impl/OrderDaoImpl.java
@@ -8,7 +8,6 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
-import java.sql.Types;
 import java.util.List;
 
 /**
@@ -19,24 +18,25 @@ import java.util.List;
  **/
 @Service
 public class OrderDaoImpl implements OrderDao {
-
     @Autowired
     JdbcTemplate jdbcTemplate;
 
 
     @Override
     public List<Order> getOrderListByUserId(Integer userId) {
-        StringBuilder sqlBuilder = new StringBuilder();
-        sqlBuilder
-                .append("select order_id, user_id from order where user_id=? ");
-        return jdbcTemplate.query(sqlBuilder.toString(), new Object[]{userId},
-                new int[]{Types.INTEGER}, new BeanPropertyRowMapper<Order>(
-                        Order.class));
+//        StringBuilder sqlBuilder = new StringBuilder();
+//        sqlBuilder
+//                .append("select order_id, user_id from order where user_id=? ");
+//        return jdbcTemplate.query(sqlBuilder.toString(), new Object[]{userId},
+//                new int[]{Types.INTEGER}, new BeanPropertyRowMapper<Order>(
+//                        Order.class));
+        return null;
+
     }
 
     @Override
     public List<Order> getOrderList() {
-        String sql = "select order_id, user_id from order ";
+        String sql = "select order_id, user_id from order_0 ";
 
         return jdbcTemplate.query(sql, new BeanPropertyRowMapper<Order>(
                 Order.class));
@@ -45,12 +45,11 @@ public class OrderDaoImpl implements OrderDao {
     @Override
     public void createOrder(Order order) {
         StringBuffer sb = new StringBuffer();
-        sb.append("insert into order(user_id, order_id)");
+        sb.append("insert into order_0(user_id, order_id)");
         sb.append("values(");
         sb.append(order.getUserId()).append(",");
         sb.append(order.getOrderId());
         sb.append(")");
         jdbcTemplate.update(sb.toString());
-
     }
 }
